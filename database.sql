740c577f344d9668393e3f658580c114f8d3a630
nadun <weerakkody.kn@gmail.com>
2025-04-17 19:28:57 +0530

    added MySQL JDBC Driver

--- database.sql
+++ database.sql
@@ -6,3 +6,13 @@ CREATE TABLE users (
        email VARCHAR(100) NOT NULL,
        password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE vendors (
+         id INT AUTO_INCREMENT PRIMARY KEY,
+         store_name VARCHAR(100) NOT NULL,
+         username VARCHAR(50) NOT NULL UNIQUE,
+         email VARCHAR(100) NOT NULL UNIQUE,
+         password VARCHAR(255) NOT NULL,
+         business_id VARCHAR(50),
+         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
