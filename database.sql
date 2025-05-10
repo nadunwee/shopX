9f771490e2913319330f6317196931321242fd17
nadun <weerakkody.kn@gmail.com>
2025-05-10 20:18:52 +0530

    fix product table

--- database.sql
+++ database.sql
@@ -6,35 +6,37 @@ CREATE TABLE users (
        email VARCHAR(100) NOT NULL,
        password VARCHAR(255) NOT NULL
 );
-# changed some columns by Aroshana
+
+-- changed some columns by Aroshana
 CREATE TABLE `products` (
-            `product_id` int(11) NOT NULL,
-            `name` varchar(255) NOT NULL,
-            `vendorID` int(11) NOT NULL,
-            `price` float(10,2) NOT NULL,
-            `productImageFileName` varchar(512) DEFAULT NULL,
-            `stock` int(11) DEFAULT 0,
-            `additional_details` text DEFAULT NULL,
-            `category` varchar(100) DEFAULT NULL,
-            `rating` float(2,1) DEFAULT 0.0,
-            `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-            `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+        `product_id` int(11) NOT NULL AUTO_INCREMENT,
+        `name` varchar(255) NOT NULL,
+        `vendorID` int(11) NOT NULL,
+        `price` float(10,2) NOT NULL,
+        `productImageFileName` varchar(512) DEFAULT NULL,
+        `stock` int(11) DEFAULT 0,
+        `additional_details` text DEFAULT NULL,
+        `category` varchar(100) DEFAULT NULL,
+        `rating` float(2,1) DEFAULT 0.0,
+        `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+        `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+         PRIMARY KEY (`product_id`)
+);
 
 CREATE TABLE cart (
-          id INT AUTO_INCREMENT PRIMARY KEY,
-          user_id INT NOT NULL,
-          product_id INT NOT NULL,
-          quantity INT NOT NULL DEFAULT 1,
-          price DECIMAL(10, 2) NOT NULL,
-          added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      user_id INT NOT NULL,
+      product_id INT NOT NULL,
+      quantity INT NOT NULL DEFAULT 1,
+      price DECIMAL(10, 2) NOT NULL,
+      added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-          FOREIGN KEY (product_id) REFERENCES products(product_id),
-          FOREIGN KEY (user_id) REFERENCES users(id)
+      FOREIGN KEY (product_id) REFERENCES products(product_id),
+      FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 
-# vendor's tables
+-- vendors tables
 
 CREATE TABLE `vendors` (
        `id` int(11) NOT NULL,
