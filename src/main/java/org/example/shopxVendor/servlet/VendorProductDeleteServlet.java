8412d74b1f8ae3cb456c2e505f7c63f270d0ed0b
AroshXR <aroshanasandeep454@gmail.com>
2025-05-20 12:16:12 +0530

    VendorProductDeleteServlet.java solved

--- src/main/java/org/example/shopxVendor/servlet/VendorProductDeleteServlet.java
+++ src/main/java/org/example/shopxVendor/servlet/VendorProductDeleteServlet.java
@@ -2,46 +2,32 @@ package org.example.shopxVendor.servlet;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
-import org.example.shopx.Checkout.AddressModel;
-import org.example.shopx.DBConnection;
 import org.example.shopxVendor.controller.VendorProductController;
-
-import java.io.File;
 import java.io.IOException;
-import java.sql.*;
-
 public class VendorProductDeleteServlet extends HttpServlet {
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+
+        int vendorID;
         HttpSession session = request.getSession(false);
-        int userId;
 
-        // Check if the user is logged in
-        if (session != null && session.getAttribute("userID") != null) {
-            userId = (Integer) session.getAttribute("userID");
+        if (session != null) {
+            vendorID = (Integer) session.getAttribute("vendorID");
         } else {
-            response.getWriter().println("<script>alert('You must be logged in to delete an address.'); window.location.href='login.jsp';</script>");
+            System.out.println("<h2>Error: You are not logged in as a vendor.</h2>");
             return;
         }
+        String name = request.getParameter("name");
+        int product_id = Integer.parseInt(request.getParameter("product_id"));
+        String productPhotosPath = getServletContext().getRealPath("/photos");
 
-        int addressID;
-
-        try {
-            addressID = Integer.parseInt(request.getParameter("addressID"));
-        } catch (NumberFormatException e) {
-            response.getWriter().println("<script>alert('Invalid address ID.'); window.location.href='checkout.jsp';</script>");
-            return;
-        }
-
-        boolean isDeleted = AddressModel.deleteAddress(addressID, userId);
+        boolean isDeleted = VendorProductController.vendorDeleteProduct(vendorID, name, product_id, productPhotosPath);
 
         if (isDeleted) {
-            response.getWriter().println("<script>alert('Address deleted successfully!'); window.location.href='checkout.jsp';</script>");
+            response.getWriter().println("<script>alert('Delete successful'); window.location.href='Vendor/vendorViewProducts.jsp';</script>");
         } else {
-            response.getWriter().println("<script>alert('Failed to delete the address.'); window.location.href='checkout.jsp';</script>");
+            response.getWriter().println("<script>alert('Item is not deleted'); window.location.href='Vendor/vendorViewProducts.jsp';</script>");
         }
-
-
     }
-
 }
